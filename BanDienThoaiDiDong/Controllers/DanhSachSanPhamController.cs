2b1beaeb8c7310282c7a5b88e1273fefec0092e1
obito0147 <118666537+obito0147@users.noreply.github.com>
2023-04-13 09:35:05 +0700

    Thêm phân trang
    thêm phân trang

--- BanDienThoaiDiDong/Controllers/DanhSachSanPhamController.cs
+++ BanDienThoaiDiDong/Controllers/DanhSachSanPhamController.cs
@@ -1,9 +1,11 @@
 ﻿using BanDienThoaiDiDong.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace BanDienThoaiDiDong.Controllers
 {
@@ -11,37 +13,64 @@ namespace BanDienThoaiDiDong.Controllers
     {
         DB_DiDongEntities database = new DB_DiDongEntities();
         // GET: DanhSachSanPham
-        public ActionResult Index(string sortOrder, string SearchString)
+        public ActionResult Index(string sortOrder, string SearchString, int? page )
         {
 
-            DanhSachSPvaTH objSanPham = new DanhSachSPvaTH();
+            //DanhSachSPvaTH objSanPham = new DanhSachSPvaTH();
 
-            objSanPham.lstHang = database.LOAISANPHAMs.ToList();
-            //sap xep
+
+            //objSanPham.lstHang = database.LOAISANPHAMs.ToList();
+            ////sap xep
+            //ViewBag.SortTheoTen = String.IsNullOrEmpty(sortOrder) ? "ma_desc" : "";
+            //if (!string.IsNullOrEmpty(SearchString))
+            //{
+            //    objSanPham.lstSanPham = database.SANPHAMs.Where(n => n.TenSP.Contains(SearchString)).ToList();
+
+            //}
+            //else
+            //{
+            //    //lay san pham trong SANPHAM
+            //    objSanPham.lstSanPham = database.SANPHAMs.ToList();
+            //}
+            //switch (sortOrder)
+            //{
+            //    case "ma_desc":
+            //        objSanPham.lstSanPham = objSanPham.lstSanPham.OrderByDescending(s => s.TenSP).ToList();
+            //        break;
+            //    default:
+            //        objSanPham.lstSanPham = objSanPham.lstSanPham.OrderBy(s => s.TenSP).ToList();
+            //        break;
+            //}
+            int pageSize = 9;
+            int pageNum = (page ?? 1);
+            var lstSanPham = database.SANPHAMs.ToList();
+            //sap xep 
             ViewBag.SortTheoTen = String.IsNullOrEmpty(sortOrder) ? "ma_desc" : "";
             if (!string.IsNullOrEmpty(SearchString))
             {
-                objSanPham.lstSanPham = database.SANPHAMs.Where(n => n.TenSP.Contains(SearchString)).ToList();
-
+                lstSanPham = database.SANPHAMs.Where(n => n.TenSP.Contains(SearchString)).ToList();
             }
             else
             {
                 //lay san pham trong SANPHAM
-                objSanPham.lstSanPham = database.SANPHAMs.ToList();
+                lstSanPham = database.SANPHAMs.ToList();
             }
             switch (sortOrder)
             {
                 case "ma_desc":
-                    objSanPham.lstSanPham = objSanPham.lstSanPham.OrderByDescending(s => s.TenSP).ToList();
+                    lstSanPham = lstSanPham.OrderByDescending(s => s.TenSP).ToList();
                     break;
                 default:
-                    objSanPham.lstSanPham = objSanPham.lstSanPham.OrderBy(s => s.TenSP).ToList();
+                    lstSanPham = lstSanPham.OrderBy(s => s.TenSP).ToList();
                     break;
             }
-            return View(objSanPham);
+            ViewBag.dsHang = database.LOAISANPHAMs.ToList();
+            return View(lstSanPham.ToPagedList(pageNum,pageSize));
         }
-        public ActionResult SanPhamTheoThuongHieu(int id, string SearchString, string sortOrder)
+        public ActionResult SanPhamTheoThuongHieu(int id, string SearchString, string sortOrder, int? page)
         {
+            int pageSize = 9;
+            int pageNum = (page ?? 1);
             ViewBag.SortTheoTen = String.IsNullOrEmpty(sortOrder) ? "ma_desc" : "";
             ViewBag.TenSortParm = sortOrder == "ten" ? "ten_desc" : "ten";
             var lstSanPham = database.SANPHAMs.Where(n => n.MaSP == id).ToList();
@@ -64,7 +93,7 @@ namespace BanDienThoaiDiDong.Controllers
                     lstSanPham = lstSanPham.OrderBy(s => s.TenSP).ToList();
                     break;
             }
-            return View(lstSanPham);
+            return View(lstSanPham.ToPagedList(pageNum,pageSize));
         }
     }
 }
