bff3cfd1b31f8bb51aa79a3c5f276790b1ebbcab
Windyyyyyyyyy <phonglagio17@gmail.com>
2023-04-14 21:35:53 +0700

    Fix

---
+++ BanDienThoaiDiDong/Areas/QLDH/QLDHAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace BanDienThoaiDiDong.Areas.QLDH
+{
+    public class QLDHAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName
+        {
+            get 
+            {
+                return "QLDH";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "QLDH_default",
+                "QLDH/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
